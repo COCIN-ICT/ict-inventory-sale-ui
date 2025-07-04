87fa121121e24a675ea74834970f83a32059ae58
maffiee <chollomvictory@gmail.com>
2025-07-04 06:46:28 +0100

    Update: latest changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {path: 'login', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
   {path: '', redirectTo: 'login', pathMatch: 'full'},
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+ 
  
   {path: '**', redirectTo: 'login'}
 ];
