a7e16733b8b1e9090ef777e0fc88855afb1e4cef
maffiee <chollomvictory@gmail.com>
2025-08-26 20:58:01 +0100

    WIP: my current changes

--- src/app/dashboard/pages/purchase-order/purchase-order.module.ts
+++ src/app/dashboard/pages/purchase-order/purchase-order.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PurchaseOrderRoutingModule } from './purchase-order-routing.module';
 import { PurchaseOrderComponent } from './purchase-order.component';
 import { PurchaseOrderListComponent } from './components/purchase-order-list/purchase-order-list.component';
+import { PurchaseOrderDetailsComponent } from './components/purchase-order-details/purchase-order-details.component';
 
 
 @NgModule({
   declarations: [
     PurchaseOrderComponent,
-    PurchaseOrderListComponent
+    PurchaseOrderListComponent,
+    PurchaseOrderDetailsComponent
   ],
   imports: [
     CommonModule,
