08aca22354f6a6130436c65dc98da1faf50ea8d1
maffiee <chollomvictory@gmail.com>
2025-10-15 23:24:36 +0100

    commit

--- src/app/dashboard/pages/store/components/store-details/store-details.component.ts
+++ src/app/dashboard/pages/store/components/store-details/store-details.component.ts
@@ -3,6 +3,9 @@ import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { StoreService } from '../../../../../services/store.service';
 import { ToastService } from '../../../../../services/toast.service';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { PricingService } from '../../../../../services/pricing.service';
+import { Pricing } from '../../../pricing/pricing.model';
 
 @Component({
   selector: 'app-store-details',
@@ -11,27 +14,43 @@ import { ToastService } from '../../../../../services/toast.service';
 })
 export class StoreDetailsComponent {
   stocks: any = {};
+  allStocks: any[] = [];
   id!: number;
   batches: any[] = [];
   promotions: any[] = [];
   pricePerUnit: number = 0;
+
+  pricingForm!: FormGroup;
+  isSubmitting = false;
+  message = '';
+  availableStock: any;
+
+  showAddPriceModal = false;
+
 
 
   // constructor(public router: Router) {
   //   const nav = this.router.getCurrentNavigation();
   //   this.stock = nav?.extras?.state?.['stock'];
   // }
-   constructor(
+  constructor(
     private route: ActivatedRoute,
     private http: HttpClient,
     private router: Router,
     private toast: ToastService,
-    private storeService: StoreService
+    private storeService: StoreService,
+    private fb: FormBuilder,
+    private pricingService: PricingService,
   ) {}
 
   ngOnInit(): void {
     this.id = Number(this.route.snapshot.paramMap.get('id'));
+    this.pricingForm = this.fb.group({
+          availableStock: ['', Validators.required],
+          price: ['', [Validators.required, Validators.min(1)]],
+        });
     this.loadStoreDetails();
+    this.loadStock();
   }
 
   loadStoreDetails() {
@@ -52,6 +71,56 @@ export class StoreDetailsComponent {
       }
     });
   }
+
+  openModal(){
+    this.showAddPriceModal = true;
+  }
+
+   closeModal() {
+    this.showAddPriceModal = false;
+  }
+
+  onUpdatePrice(){}
+
+  loadStock(): void {
+    this.pricingService.getStock().subscribe({
+      next: (stockList) => (this.allStocks = stockList),
+      error: (err) => console.log('Failed to load stock', err),
+    });
+  }
+
+
+   onSubmit(): void {
+    if (this.pricingForm.invalid) return;
+
+    this.isSubmitting = true;
+    this.message = '';
+
+    const payload: Pricing = {
+      stockId: this.pricingForm.value.availableStock,
+      price: this.pricingForm.value.price,
+    };
+
+    this.pricingService.createPricing(payload).subscribe({
+      next: () => {
+        this.toast.success('Pricing created successfully');
+        this.pricingForm.reset();
+        this.isSubmitting = false;
+        this.closeModal(); // 👈 close modal after success
+        this.loadStoreDetails();
+      },
+      error: (err) => {
+        this.toast.error(err.error.message);
+        this.isSubmitting = false;
+      },
+    });
+  }
+
+
+
+
+
+
 
   
 
--- src/app/dashboard/pages/store/components/store-list/store-list.component.ts
+++ src/app/dashboard/pages/store/components/store-list/store-list.component.ts
@@ -148,4 +148,5 @@ openEditForm(store: Store): void {
 
 
 
+
 }
